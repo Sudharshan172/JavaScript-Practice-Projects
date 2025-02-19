6c0d1ee26d63ebd6cd206ad9f365d5ab503d10af
Sudharshan172 <sudharshanreddy.l1729@gmail.com>
2025-02-19 16:11:29 +0530

    updated codes

--- 01.bulb_on_off/bulb_on_off.js
+++ 01.bulb_on_off/bulb_on_off.js
@@ -1,11 +1,14 @@
 function switchoff(){
     document.getElementById("bulbOffImage").src="https://image.ibb.co/hoBxtm/light_bulb_off.png";
     document.getElementById("btn-off").style.backgroundColor="red";
-    document.getElementById("btn-on").style.backgroundColor="";
+    document.getElementById("btn-off").style.color="white";
+    document.getElementById("btn-on").style.backgroundColor="white";
+    document.getElementById("btn-on").style.color="black";
 };
-
 function switchon(){
     document.getElementById("bulbOffImage").src="https://image.ibb.co/cBBaeR/light_bulb_on.png";
     document.getElementById("btn-on").style.backgroundColor="green";
-    document.getElementById("btn-off").style.backgroundColor="";
+    document.getElementById("btn-on").style.color="white";
+    document.getElementById("btn-off").style.backgroundColor="white";
+    document.getElementById("btn-off").style.color="black";
 }
--- 07.season_switcher/season_switcher.js
+++ 07.season_switcher/season_switcher.js
@@ -1,12 +1,12 @@
 function spring(){
     document.getElementById("seasonsImage").src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-autumn-xs-img.png";
 }
-function summer(){
+function winter(){
     document.getElementById("seasonsImage").src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-winter-xs-img.png";
 }
 function autmn(){
     document.getElementById("seasonsImage").src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-spring-xs-img.png";
 }
-function winter(){
+function summer(){
     document.getElementById("seasonsImage").src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-summer-xs-img.png";
 }
